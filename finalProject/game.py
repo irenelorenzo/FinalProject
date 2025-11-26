010398355625c38ba5ec0aafc30812418db35e0e
irenelorenzo <100583468@alumnos.uc3m.es>
2025-11-26 09:49:41 +0100

    Adding stuff

---
+++ finalProject/game.py
@@ -0,0 +1,28 @@
+import pyxel
+
+class Game:
+    def __init__(self):
+        pyxel.init(240, 240)
+        pyxel.load("my_resource.pyxres")
+
+        self.x = 40
+        self.y = 40
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.x -= 1
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.x += 1
+        if pyxel.btn(pyxel.KEY_UP):
+            self.y -= 1
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.y += 1
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.bltm(0, 0, 0, 0, 0, 240, 240)     # draw tilemap
+        pyxel.blt(self.x, self.y, 0, 0, 0, 8, 8, 0)  # draw player sprite
+
+Game()
