098a7832e0a6c95b46b77d33bd8f8bc69193e628
irenelorenzo <100583468@alumnos.uc3m.es>
2025-11-30 18:10:59 +0100

    Moving characters!

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,5 +1,6 @@
 import pyxel
 from player import Player
+from truck import Truck
 
 pyxel.init(256, 128)
 pyxel.load("../assets/my_resource.pyxres")
@@ -9,6 +10,8 @@ pyxel.bltm(0, 0, 0, 0, 0, 256, 128)
 
 mario = Player("right")
 luigi = Player("left")
+truck = Truck()
+
 
 def update():
     # Use this function to alter mario and luigi's position (add conditions for when 8 packages at truck, etc.)
@@ -20,5 +23,6 @@ def draw():
     pyxel.bltm(0, 0, 0, 0, 0, 256, 128) # Redraw the tilemap
     mario.draw() # Draw Mario at its new position
     luigi.draw() # Draw Luigi at its new position
+    truck.draw()
 
 pyxel.run(update, draw) #run the program
--- finalProject/truck.py
+++ finalProject/truck.py
@@ -1,10 +1,40 @@
+import pyxel
 class Truck:
     """This is a class to represent the truck. In the game, there will only be one truck. When there are 8 packages in
     the truck, it will move out of frame and become the initial picture again """
     # First, we will define the __init__ method to initialise the attributes needed for the truck
     def __init__(self):
-        """This is the __init__ function for the truck
-        :param side: str. A string representing the side each of the characters is in. Mario will be on the right side
-        of the screen and Luigi on the left. It will determine the x-coordinates of the characters, as well as the
-        sprite used for them
-        """
+        """This is the __init__ function for the truck"""
+        self.package_count = 0
+        self.image = 0
+        self.x = 0
+        self.y = 56
+
+    @property
+    def y(self):
+      return self.__y
+
+    @y.setter
+    def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError("The y-coordinate must be an integer")
+        # no condition established for the bounds of the value of y, since they are established in the init function
+        else:
+            self.__y = y
+
+    def added_package(self):
+        """This is a method for when a new package reaches the truck"""
+        self.package_count += 1
+        self.image += 32
+        if self.package_count == 8:
+            for _ in range(48): # When the truck has eight packages, move truck out of screen 1 pixel at a time
+                self.x -= 1
+            self.package_count = 0
+            self.image = 0
+            for _ in range(48): # Bring truck back into screen
+                self.x += 1
+
+
+    def draw(self):
+        """This method will display the truck on the screen"""
+        pyxel.blt(self.x, self.y, 1, 0, self.image, 64, 40, 0)
