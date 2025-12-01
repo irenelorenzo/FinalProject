c43a65f7839684530848cbf43d4e26ab1f07cd1b
irenelorenzo <100583468@alumnos.uc3m.es>
2025-12-01 18:47:31 +0100

    Editing package

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,6 +1,7 @@
 import pyxel
 from player import Player
 from truck import Truck
+from package import Package
 
 pyxel.init(256, 128)
 pyxel.load("../assets/my_resource.pyxres")
@@ -11,7 +12,7 @@ pyxel.bltm(0, 0, 0, 0, 0, 256, 128)
 mario = Player("right")
 luigi = Player("left")
 truck = Truck()
-
+package0 = Package(0, "right")
 
 def update():
     # Use this function to alter mario and luigi's position (add conditions for when 8 packages at truck, etc.)
--- finalProject/package.py
+++ finalProject/package.py
@@ -1,15 +1,18 @@
 
 class Package:
-
-    def __init__(self,direction:str, at_truck:bool):
-        self.level=0
-        self.position=0
+    def __init__(self, conveyor: int, direction:str):
+        self.conveyor = 0
+        self.position = 0
         self.direction = direction
         self.at_truck = False
-        if self.direction=="right":
-            self.image="Package_Right.png"
+        if self.conveyor == 0 or (self.conveyor//2) == 0:
+            self.direction = "left"
         else:
-            self.image="Package_Left.png"
+            self.direction = "right"
+        self.x_image = # Add x coordinates of package sprites
+        self.y_image =  # Add y coordinates of package sprites (later on, add a 16 to this attribute when middle of belt is reached)
+
+
     @property
     def direction(self)->str:
         return self.__direction
@@ -17,6 +20,9 @@ class Package:
     @property
     def at_truck(self)->bool:
         return self.at_truck
+
+    @property
+    def
 
     @direction.setter
     def direction(self,direction:str):
