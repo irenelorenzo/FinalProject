a7dbb8c38db1da2dbff66f53a9669fcf60647fb7
irenelorenzo <100583468@alumnos.uc3m.es>
2025-12-06 09:23:42 +0100

    updating changes

--- finalProject/main.py
+++ finalProject/main.py
@@ -8,10 +8,10 @@
 
 # Import all the classes, and pyxel
 import pyxel
-from player import Player
-from truck import Truck
-from newpackage import Package
-from newconveyor import Conveyor
+from finalProject.player import Player
+from finalProject.truck import Truck
+from finalProject.newpackage import Package
+from finalProject.newconveyor import Conveyor
 
 # Set the scene with the tilemap
 pyxel.init(256, 128)
@@ -36,6 +36,7 @@ conveyor2 = Conveyor("even", 82)
 conveyor3 = Conveyor("odd", 66)
 conveyor4 = Conveyor("even", 50)
 conveyor5 = Conveyor("odd", 34)
+conveyors = [conveyor1, conveyor2]
 
 def update():
     # Use this function to alter mario and luigi's position (add conditions for when 8 packages at truck, etc.)
@@ -45,6 +46,7 @@ def update():
 
     package.update(conveyor.x, conveyor.limit, mario.level, True)
     # Use for loops to update conveyors to simplify everything
+
     conveyor1.update(package.collision)
     package1.update(conveyor1.x, conveyor1.limit, luigi.level, package.collision)
     conveyor2.update(package1.collision)
